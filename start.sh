11ef62557e6ac01e69666dafe0270244c66281e5
hyoniel2025-dot <hyoniel2025@gmail.com>
2026-03-23 10:50:02 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-# instalar 7zip
+# ⚡ Actualizar paquetes e instalar 7zip
 apt-get update && apt-get install -y p7zip-full
 
-# iniciar servidor con Gunicorn + Uvicorn
+# ⚡ Iniciar servidor FastAPI con Gunicorn + Uvicorn
+#    main:app → archivo main.py, variable app de FastAPI
 exec gunicorn -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:$PORT --workers 1
